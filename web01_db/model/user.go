020df7b879e910f7528304500a13d3fd767e8a2c
OhMimi <gash60535@gmail.com>
2020-06-14 19:32:02 +0800

    [update]db queery users

--- web01_db/model/user.go
+++ web01_db/model/user.go
@@ -44,3 +44,68 @@ func (user *User) AddUser2() error {
 	}
 	return nil
 }
+
+// GetUserById 根據用戶的id從數據庫中查詢一筆紀錄
+func (user *User) GetUserById(id int) (findUser *User, err error) {
+	// 1.寫sql語句
+	sqlStr := "select id,username,password,email from users where id = ?"
+	// 2.執行
+	row := utils.Db.QueryRow(sqlStr, user.ID)
+	// 聲明變量
+	var (
+		username string
+		password string
+		email    string
+	)
+
+	err = row.Scan(&id, &username, &password, &email)
+	if err != nil {
+		fmt.Println("row.Scan() err = ", err)
+		return
+	}
+	// 創建一個USER實例,
+	findUser = &User{
+		ID:       id,
+		Username: username,
+		Password: password,
+		Email:    email,
+	}
+
+	return
+}
+
+// GetUsers 從數據庫中查詢所有紀錄
+func (user *User) GetUsers() (findUsers []*User, err error) {
+	// 1.寫sql語句
+	sqlStr := "select id,username,password,email from users"
+	rows, err := utils.Db.Query(sqlStr)
+	if err != nil {
+		fmt.Println("utils.Db.Query() err = ", err)
+		return
+	}
+	// rows.Next 可查看下一筆資料是否存在
+	for rows.Next() {
+		// 聲明變量
+		var (
+			id       int
+			username string
+			password string
+			email    string
+		)
+		err = rows.Scan(&id, &username, &password, &email)
+		if err != nil {
+			fmt.Println("rows.Scan() err = ", err)
+			return
+		}
+		// 創建一個USER實例,
+		findUser := &User{
+			ID:       id,
+			Username: username,
+			Password: password,
+			Email:    email,
+		}
+		findUsers = append(findUsers, findUser)
+	}
+
+	return
+}
--- web01_db/model/user_test.go
+++ web01_db/model/user_test.go
@@ -2,19 +2,90 @@ package model
 
 import (
 	"fmt"
+	"os"
 	"testing"
 )
 
-func TestAddUser(t *testing.T) {
+// 簡易測試指令: go test
+// 詳細測試指令: go test -v
+
+// TestMain 函數可以在測試函數執行前做一些其他操作
+func TestMain(m *testing.M) {
+	fmt.Println("測試開始:")
+	// 透過m.Run()來執行測試函數
+	m.Run()
+	os.Exit(0)
+}
+
+func TestUser(t *testing.T) {
+	fmt.Println("開始測試User中的相關方法")
+	// 透過t.Run()來執行子測試程序
+	t.Run("測試添加用戶: ", testAddUser)
+	t.Run("測試獲取一筆用戶: ", testGetUserById)
+	t.Run("測試獲取所有用戶: ", testGetUsers)
+}
+
+// 如果函數名不是以TestXxx開頭,那該函數不執行
+func testAddUser(t *testing.T) {
 	fmt.Println("測試添加用戶: ")
-	user := &User{}
+	// user := &User{}
 	// 調用添加用戶的方法
-	err := user.AddUser()
-	if err != nil {
-		fmt.Println("user.AddUser 有錯誤 err = ", err)
+	// err := user.AddUser()
+	// if err != nil {
+	// 	fmt.Println("user.AddUser 有錯誤 err = ", err)
+	// return
+	// }
+	// err = user.AddUser2()
+	// if err != nil {
+	// 	fmt.Println("user.AddUser2 有錯誤 err = ", err)
+	// return
+	// }
+}
+
+// // 如果函數名不是以TestXxx開頭,那該函數不執行
+// func TestAddUser(t *testing.T) {
+// 	fmt.Println("測試添加用戶: ")
+// 	user := &User{}
+// 	// 調用添加用戶的方法
+// 	err := user.AddUser()
+// 	if err != nil {
+// 		fmt.Println("user.AddUser 有錯誤 err = ", err)
+// return
+// 	}
+// 	err = user.AddUser2()
+// 	if err != nil {
+// 		fmt.Println("user.AddUser2 有錯誤 err = ", err)
+// return
+// 	}
+// }
+
+// 測試獲取一筆用戶紀錄
+func testGetUserById(t *testing.T) {
+	fmt.Println("測試查詢一筆紀錄: ")
+	user := &User{
+		ID: 1,
 	}
-	err = user.AddUser2()
+	// 調用獲取User的方法
+	findUser, err := user.GetUserById(user.ID)
 	if err != nil {
-		fmt.Println("user.AddUser2 有錯誤 err = ", err)
+		fmt.Println("user.GetUserById 有錯誤 err = ", err)
+		return
+	}
+	fmt.Printf("得到的User信息是: ID=%d 用戶名=%s 密碼=%s email=%s\n", findUser.ID, findUser.Username, findUser.Password, findUser.Email)
+}
+
+// 測試獲取所有用戶紀錄
+func testGetUsers(t *testing.T) {
+	fmt.Println("測試查詢所有紀錄: ")
+	user := &User{}
+	// 調用 獲取所有用戶方法
+	users, err := user.GetUsers()
+	if err != nil {
+		fmt.Println("user.GetUsers 有錯誤 err = ", err)
+		return
+	}
+	// 遍歷切片
+	for i, u := range users {
+		fmt.Printf("得到的第%d個User信息是: ID=%d 用戶名=%s 密碼=%s email=%s\n", i+1, u.ID, u.Username, u.Password, u.Email)
 	}
 }
